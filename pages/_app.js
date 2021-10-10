6ae140cd8f5be3a213e5dd461e55bcd57e3b83e3
Toto-lonardo <s.lonardo87@gmail.com>
2021-10-10 14:37:26 +0200

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import 'tailwindcss/tailwind.css'
+//import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="mx-auto w-10/12 my-8">
+      <header>
+        <h1 className="text-6xl font-bold text-center filter drop-shadow">
+          My Blog
+        </h1>
+        <nav className="my-4">
+          <ul className="flex flex-row space-x-4 justify-center">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-export default MyApp
+export default MyApp;
