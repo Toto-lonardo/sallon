718fc7377bc856211fd3b9ccd7948cc048030baa
Toto-lonardo <s.lonardo87@gmail.com>
2022-11-21 01:14:31 +0100

    new logo

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,9 @@ export default function Home() {
     transition: { duration: 1, delay:4 }, }}
  whileTap={{ scale: 0.6 }}
 whileInView={{ opacity: 1}}
-    transition={{ type: "spring", stiffness: 100, damping: 12}} className="text-6xl text-giallo text-font-sans ">s<span className="text-celeste font-bold ">All</span><span className=" text-verde font-bold text-5xl inline-block align-top mt-1">()</span>n</motion.h2>
+    transition={{ type: "spring", stiffness: 100, damping: 12}} className="text-5xl text-verde antialiased text-font-sans tracking-tighter">S.<span className="text-celeste ">all</span><span className=" text-giallo text-4xl inline-block  align-[0.5rem]">(<span className="text-verde text-5xl align-[-0.5rem] font-normal ">On</span>)</span></motion.h2>
 
-              <p className="mt-2 text-giallo text-sm">Sicilian <span className="text-celeste">Addicted to </span><span className="text-verde">Coding</span></p>
+              <p className="mt-2 text-verde tracking-tighter antialiased ">Sicilian <span className="text-celeste">Addicted<span className="text-giallo"> to </span></span><span className="text-verde">Coding</span></p>
             </div>
         </div>
           {/* <AnimatedTextCharacter text="hello world" /> */}
