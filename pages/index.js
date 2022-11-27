e37425449d103dcb12d0cdffcba40029ded2db1e
Toto-lonardo <s.lonardo87@gmail.com>
2022-11-27 18:31:11 +0100

    fix margin on cards

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
       <main className=" h-screen bg-sfondo flex flex-col items-center justify-center">
         <div className="container mx-auto ">
-          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl mx-auto shadow-celeste/40 shadow-xl rounded border-celeste border-4">
+          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-xl rounded border-celeste border-4 mx-8 ">
             <div className=" basis-1/2">
               <Image src={profilo} width={"auto"} height={"auto"} alt={"Sallon alias Salvatore Lo Nardo"} className="mx-auto " />
             </div>
