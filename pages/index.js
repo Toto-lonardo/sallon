4f978699431060720d997484634adc1c6d15036b
Toto-lonardo <s.lonardo87@gmail.com>
2022-12-03 18:57:30 +0100

    card components

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 // import AnimatedTextCharacter from "../components/AnimatedTextCharacter"
 import profilo from "../public/sallon.jpg"
 import Image from "next/image";
+import Card from "../components/card"
 
 export default function Home() {
   return (
@@ -11,9 +12,9 @@ export default function Home() {
         <title>Sallon Sicilian Proud Dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" h-screen bg-sfondo flex flex-col items-center justify-center">
+      <main className=" bg-sfondo flex flex-col items-center justify-center">
         <div className="container mx-auto ">
-          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-xl rounded border-celeste border-4 mx-8 ">
+          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-xl rounded border-celeste border-4 md:mx-auto mx-8 my-16 ">
             <div className=" basis-1/2">
               <Image src={profilo} width={"auto"} height={"auto"} alt={"Sallon alias Salvatore Lo Nardo"} className="mx-auto " />
             </div>
@@ -35,6 +36,9 @@ export default function Home() {
               </div>
             </div>
           </div>
+            <Card children={"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."} title={"About me"} />
+            <Card children={"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."} title={"Portfolio"} />
+            <Card children={"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."} title={"Contacts"} />
         </div>
         {/* <AnimatedTextCharacter text="hello world" /> */}
       </main>
