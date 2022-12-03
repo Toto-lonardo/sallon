2e10131dc54f89010fe725a7ad210a6bed30f4b4
Toto-lonardo <s.lonardo87@gmail.com>
2022-12-03 19:08:02 +0100

    fixing

--- components/card.js
+++ components/card.js
@@ -3,7 +3,7 @@ export default function Card({ children, title }) {
     <>
       <div className="bg-sky-800 flex-col flex max-w-3xl shadow-celeste/40 shadow-xl rounded border-celeste border-4 md:mx-auto mx-8 my-16">
         <div className="text-giallo text-3xl mx-auto my-2">{title}</div>
-        <div>{children}</div>
+        {children}
       </div>
     </>
 
