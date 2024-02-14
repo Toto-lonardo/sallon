0d7115a5c2f4d5490918618a20cfa101ae5188ab
Toto-lonardo <s.lonardo87@gmail.com>
2024-02-14 19:49:25 +0100

    Adding text animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
-// import AnimatedTextCharacter from "../components/AnimatedTextCharacter"
+import { FaGithub, FaLinkedin, FaTelegram } from "react-icons/fa";
+import { ReactTyped } from "react-typed";
 import profilo from "../public/sallon.jpg";
 import Image from "next/image";
 
@@ -17,7 +18,7 @@ export default function Home() {
                 className="mx-auto "
               />
             </div>
-            <div className="flex flex-col items-center justify-center  mx-auto flex-auto  basis-1/2 p-10">
+            <div className="flex flex-col items-center justify-center  mx-auto flex-auto gap-2  basis-1/2 p-10">
               <motion.h2
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
@@ -37,13 +38,37 @@ export default function Home() {
                 Salvatore <span className="text-giallo">Lo Nardo</span>
               </p>
               <div className="tracking-tighter font-semibold text-slate-300">
-                <hr className=" border-giallo my-2"></hr>
-                <h3>Front-end Web Developer</h3>
+                <div className="flex h-6 w-56 justify-center items-center">
+                  <ReactTyped
+                    strings={[
+                      "Proud Linux lover",
+                      "Extremely Vim addicted",
+                      "Frontend Web Developer",
+                    ]}
+                    typeSpeed={40}
+                    backSpeed={30}
+                    cursorChar=""
+                    className=""
+                  />
+                </div>
+                <div className="my-4 flex gap-6 justify-center items-center ">
+                  <a href="https://github.com/Toto-lonardo" target="_blank">
+                    <FaGithub className="size-8" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/salvatore-lo-nardo-813233b4/"
+                    target="_blank"
+                  >
+                    <FaLinkedin className="size-8" />
+                  </a>
+                  <a href="https://t.me/sallon87" target="_blank">
+                    <FaTelegram className="size-8" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <AnimatedTextCharacter text="hello world" /> */}
       </main>
     </>
   );
