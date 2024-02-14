a619b17ea35847c12d4924bba2461d19c66bbf4f
Toto-lonardo <s.lonardo87@gmail.com>
2024-02-14 17:39:37 +0100

    Fixing shadow

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <main className=" h-screen bg-sfondo flex flex-col items-center justify-center">
         <div className="container mx-auto ">
-          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-xl rounded border-celeste border-4 mx-auto ">
+          <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-lg rounded border-celeste border-4 mx-auto ">
             <div className=" basis-1/2 ">
               <Image
                 src={profilo}
