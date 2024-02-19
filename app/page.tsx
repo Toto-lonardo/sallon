3cff3e50add5c00e4b88b29f3db26be30587e534
Toto-lonardo <s.lonardo87@gmail.com>
2024-02-19 18:16:02 +0100

    Fix max height container

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <main className=" h-dvh bg-sfondo flex flex-col items-center justify-center">
-        <div className="container mx-auto ">
+        <div className="container mx-auto max-h[80vh] ">
           <div className=" bg-sky-800 md:flex-row flex-col flex max-w-3xl shadow-celeste/40 shadow-lg rounded border-celeste border-4 md:mx-auto m-6  ">
             <Tile />
           </div>
