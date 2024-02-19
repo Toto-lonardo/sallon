f6ddef67dd17d27ede26d301ff802dc52b4c95af
Toto-lonardo <s.lonardo87@gmail.com>
2024-02-19 19:06:00 +0100

    Fix min height bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Tile from "./tile";
 export default function Home() {
   return (
     <>
-      <main className=" h-dvh bg-sfondo flex flex-col items-center justify-center p-4">
+      <main className=" min-h-dvh bg-sfondo flex flex-col items-center justify-center">
         <div className="container mx-auto ">
           <div className=" bg-sky-800 md:flex-row flex-col flex max-w-md md:max-w-3xl shadow-celeste/40 shadow-lg rounded border-celeste border-4 md:mx-auto m-6  ">
             <Tile />
